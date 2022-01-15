c7337e94d90badaa7de260d6d57a1c2f86c4aaca
Nichlos Ho <hon1@seattleu.edu>
2022-01-15 15:44:06 -0800

    Added Value attribute to ItemModel.cs Added Value attribute to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -7,5 +7,8 @@ namespace Mine.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+
+        //The value of the item +9 damage
+        public int Value { get; set; } = 0;
     }
 }
