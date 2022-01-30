252eacf10a8d14243d86e303ddae222faa715be5
Nichlos Ho <hon1@seattleu.edu>
2022-01-30 11:48:51 -0800

    Added Constants class to the Shared Project, Constants.cs

---
+++ Mine/Mine/Constants.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.IO;
+
+namespace Mine
+{
+    public static class Constants
+    {
+        public const string DatabaseFileName = "TodoSQLLite.db3";
+
+        public const SQLiteOpenFlags Flags = 
+            //open database in read write mode
+            SQLiteOpenFlags.ReadWrite | 
+            //create database if it doesnt exist
+            SQLiteOpenFlags.Create | 
+            //enable multi threaded database access
+            SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get 
+            { 
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFileName);
+            }
+        }
+
+    }
+}
