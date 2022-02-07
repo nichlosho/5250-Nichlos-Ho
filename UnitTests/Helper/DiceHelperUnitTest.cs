c5a8fcbde519f699a26a7d92bb7079d724172ff2
Nichlos Ho <hon1@seattleu.edu>
2022-02-06 17:41:21 -0800

    added RollDice_Invalid_Dice_Zero UT in DiceHelperUnitTest.cs

--- UnitTests/Helper/DiceHelperUnitTest.cs
+++ UnitTests/Helper/DiceHelperUnitTest.cs
@@ -44,5 +44,17 @@ namespace UnitTests.Helpers
             // Assert 
             Assert.AreEqual(1, result);
         }
+
+        [Test]
+        public void RollDice_Invalid_Dice_Zero_Should_Return_Zero()
+        {
+            // Arrange
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
     }
 }
