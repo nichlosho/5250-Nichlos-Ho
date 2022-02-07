11290797b81244765daf1bbcfd7cfe87d11d584b
Nichlos Ho <hon1@seattleu.edu>
2022-02-06 17:39:02 -0800

    added RollDice_Invalid_Roll_Forced_1 UT to DiceHelperUnitTest.cs

--- UnitTests/Helper/DiceHelperUnitTest.cs
+++ UnitTests/Helper/DiceHelperUnitTest.cs
@@ -16,6 +16,7 @@ namespace UnitTests.Helpers
             // Assert 
             Assert.AreEqual(0, result);
         }
+
         [Test]
         public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_and_6()
         {
@@ -28,5 +29,20 @@ namespace UnitTests.Helpers
             Assert.AreEqual(true, result <= 6);
         }
 
+        [Test]
+        public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
+        {
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 1;
+
+            // Act
+            var result = DiceHelper.RollDice(1, 1);
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert 
+            Assert.AreEqual(1, result);
+        }
     }
 }
