744a35a7173c89ed8fbbf2e0abc2a11e22dc923b
Nichlos Ho <hon1@seattleu.edu>
2022-02-06 17:44:35 -0800

    Added RollDice_Valid_Roll_2_Dice_6 UT in DiceHelperUnitTest.cs

--- UnitTests/Helper/DiceHelperUnitTest.cs
+++ UnitTests/Helper/DiceHelperUnitTest.cs
@@ -10,9 +10,12 @@ namespace UnitTests.Helpers
         public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
         {
             // Arrange
+
             // Act
             var result = DiceHelper.RollDice(0, 1);
+
             // Reset
+
             // Assert 
             Assert.AreEqual(0, result);
         }
@@ -21,9 +24,12 @@ namespace UnitTests.Helpers
         public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_and_6()
         {
             // Arrange
+
             // Act
             var result = DiceHelper.RollDice(1, 6);
+
             // Reset
+
             // Assert 
             Assert.AreEqual(true, result >= 1);
             Assert.AreEqual(true, result <= 6);
@@ -38,6 +44,7 @@ namespace UnitTests.Helpers
 
             // Act
             var result = DiceHelper.RollDice(1, 1);
+
             // Reset
             DiceHelper.ForceRollsToNotRandom = false;
 
@@ -49,12 +56,28 @@ namespace UnitTests.Helpers
         public void RollDice_Invalid_Dice_Zero_Should_Return_Zero()
         {
             // Arrange
+
             // Act
             var result = DiceHelper.RollDice(1, 0);
+
             // Reset
 
             // Assert 
             Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void RollDice_Valid_Roll_2_Dice_6_Should_Return_Between_2_and_12()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(2, 6);
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(true, result >= 2);
+            Assert.AreEqual(true, result <= 12);
         }
     }
 }
